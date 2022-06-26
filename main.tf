712cc10afffb9adf5d20e0131c163dc8fc4d892e
cr-trojan23 <pandusrinu1604@gmail.com>
2022-06-26 20:42:10 +0530

    Fix: Corrected acl bucket name

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_s3_bucket" "s3bucket" {
 }
 
 resource "aws_s3_bucket_acl" "s3acl" {
-  bucket = aws_s3_bucket.s3bucket
+  bucket = aws_s3_bucket.s3bucket.bucket
   acl    = "private"
 }
 
