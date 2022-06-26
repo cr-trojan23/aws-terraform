4bb9254a29e7be3205890544a2e753ed34263b31
cr-trojan23 <pandusrinu1604@gmail.com>
2022-06-26 21:04:40 +0530

    Added S3 access blocking

--- main.tf
+++ main.tf
@@ -43,12 +43,12 @@ resource "aws_s3_bucket_acl" "s3acl" {
 }
 
 resource "aws_s3_bucket_public_access_block" "blocks3access" {
-  bucket = aws_s3_bucket.s3bucket.id
-  block_public_acls = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.s3bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
   restrict_public_buckets = true
+  ignore_public_acls      = true
 }
-
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
