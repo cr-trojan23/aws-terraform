db0cadd18d8c3be15fdf05b6aa7d4ae29811ed6e
cr-trojan23 <pandusrinu1604@gmail.com>
2022-06-27 16:58:22 +0530

    Removed unnecessary tags

--- main.tf
+++ main.tf
@@ -39,10 +39,6 @@ resource "aws_s3_bucket" "s3bucket" {
 resource "aws_s3_bucket_acl" "s3acl" {
   bucket = aws_s3_bucket.s3bucket.id
   acl    = "private"
-  tags   = {
-    "Name"      = "Sample bucket ACL",
-    "CreatedBy" = "Terraform"
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "blocks3access" {
@@ -51,10 +47,6 @@ resource "aws_s3_bucket_public_access_block" "blocks3access" {
   block_public_policy     = true
   restrict_public_buckets = true
   ignore_public_acls      = true
-  tags                   = {
-    "Name"      = "Sample bucket public access block",
-    "CreatedBy" = "Terraform"
-  }
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
